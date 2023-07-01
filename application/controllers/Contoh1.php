4c94dba36b7545a4792d58895138465fc0f845ec
salsabilaaaa9 <amaliaslsabla@gmail.com>
2023-07-01 16:48:41 +0700

    TUGASWP3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Amalia Salsabila
+        saya tinggal di Jakarta Selatan
+        Olahraga yang saya sukai adalah
+        Badminton"; 
+    } 
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,16 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama : Amalia Salsabila</li>
+    </ul> 
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Rasamala III, Jakarta Selatan</li>
+    </ul>
+    <h4>Tempat/Tanggal Lahirr</h4>
+    <ul type="none">
+        <li>Jakarta, 12 September 2003</li>
+    </ul>
+</section>
